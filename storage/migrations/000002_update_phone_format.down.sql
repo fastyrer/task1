9d11450013e11a711fe16e912b251f40f62287fc
Fasty <fasty.v5@mail.ru>
2026-07-15 08:52:51 +0300

    bd migrations fix

--- storage/migrations/000002_update_phone_format.down.sql
+++ storage/migrations/000002_update_phone_format.down.sql
@@ -1,2 +1,13 @@
+-- Сначала снимаем новое ограничение, затем возвращаем номера к формату
+-- +7 (XXX) XXX-XX-XX и восстанавливаем прежний CHECK.
 ALTER TABLE contacts DROP CONSTRAINT IF EXISTS chk_contacts_phone;
+
+UPDATE contacts
+SET phone = regexp_replace(
+	right(regexp_replace(phone, '\D', '', 'g'), 10),
+	'^([0-9]{3})([0-9]{3})([0-9]{2})([0-9]{2})$',
+	'+7 (\1) \2-\3-\4'
+)
+WHERE phone !~ '^\+7 \([0-9]{3}\) [0-9]{3}-[0-9]{2}-[0-9]{2}$';
+
 ALTER TABLE contacts ADD CONSTRAINT chk_contacts_phone CHECK (phone ~ '^\+7 \([0-9]{3}\) [0-9]{3}-[0-9]{2}-[0-9]{2}$');
--- storage/migrations/000002_update_phone_format.up.sql
+++ storage/migrations/000002_update_phone_format.up.sql
@@ -1,2 +1,9 @@
+-- Сначала снимаем старое ограничение, иначе оно отклонит номера,
+-- пока мы переводим их в плоский формат +7XXXXXXXXXX.
 ALTER TABLE contacts DROP CONSTRAINT IF EXISTS chk_contacts_phone;
+
+UPDATE contacts
+SET phone = '+7' || right(regexp_replace(phone, '\D', '', 'g'), 10)
+WHERE phone !~ '^\+7[0-9]{10}$';
+
 ALTER TABLE contacts ADD CONSTRAINT chk_contacts_phone CHECK (phone ~ '^\+7[0-9]{10}$');
